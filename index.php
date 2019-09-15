1f187d1d4d25ad9f8f61ba299a9e0a583a10fc41
MuhammadTahaAzam1997 <muhammadtahaazam@gmail.com>
2019-09-15 11:50:16 -0700

    Working on basic concepts php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Assignment #01</title>
+</head>
+<body>
+    <?php
+     
+     $flag=0;
+     for($i=100; $i < 500;++$i){
+        if($i % 2 != 0){
+            echo " ".$i;
+        }
+        
+     }
+    ?>
+</body>
+</html>
